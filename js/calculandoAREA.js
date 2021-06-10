325f177b802c3e7bdfc2c065e2c825ffb3225998
Leonardo Araújo <araujoleonardo310@gmail.com>
2021-06-09 23:20:10 -0300

    Deletando os arquivos e fazendo que tudo seja por link javascript

---
+++ js/calculandoAREA.js
@@ -0,0 +1,5 @@
+alert("Vamos calcular a área de um terreno.");
+var a = prompt("Digite a largura em metros:");
+var b = prompt("Digite o comprimento, em metros:");
+var s = a * b;
+document.write("A área do terreno é de " + s + " metros.");
